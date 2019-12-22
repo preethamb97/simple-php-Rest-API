b86d162221fe236941c7aca2674b4f5dbe54f93d
preetham b <preethamb97@gmail.com>
2019-12-22 11:24:12 +0530

    commeneted

--- main/links/selectLinks.php
+++ main/links/selectLinks.php
@@ -1,21 +1,21 @@
 <?php
 // http://localhost/aws/main/links/selectLinks.php?key=encryption
-include_once dirname(__FILE__)."/../../init.php";
-$userLib = autoload::loadLibrary('internal', 'user');
-$linkLib = autoload::loadLibrary('internal', 'links');
-$key = $_GET['key'];
-$result = array();
-$isVerified = autoload::verifyKey($key);
-if($isVerified) {
-  $linkData = $linkLib->getLinkDetail();
-  $result = $linkData;
-  $jsonFormat = json_encode($result, JSON_NUMERIC_CHECK);
-  print_r($jsonFormat);  
-} else {
+// include_once dirname(__FILE__)."/../../init.php";
+// $userLib = autoload::loadLibrary('internal', 'user');
+// $linkLib = autoload::loadLibrary('internal', 'links');
+// $key = $_GET['key'];
+// $result = array();
+// $isVerified = autoload::verifyKey($key);
+// if($isVerified) {
+//   $linkData = $linkLib->getLinkDetail();
+//   $result = $linkData;
+//   $jsonFormat = json_encode($result, JSON_NUMERIC_CHECK);
+//   print_r($jsonFormat);  
+// } else {
   
-  $result = [];
+//   $result = [];
 
-  $jsonFormat = json_encode($result, JSON_NUMERIC_CHECK);
-  print_r($jsonFormat);  
-}
+//   $jsonFormat = json_encode($result, JSON_NUMERIC_CHECK);
+//   print_r($jsonFormat);  
+// }
 ?>
